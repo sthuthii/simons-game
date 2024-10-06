1d6ff7ff257163683915212fbbe65f5e035c24c4
Sthuthi Poojary <sthuthim870@gmail.com>
2024-10-06 11:01:08 +0530

    second

--- index.js
+++ index.js
@@ -4,3 +4,25 @@ let playerSequence = [];
 let currentStep = 0;
 let gameActive = false;
 
+const colorButtons = {
+    red : document.getElementById("red"),
+    green : document.getElementById("green"),
+    blue : $("blue"),
+    yellow : document.getElementById("yellow")
+};
+
+$(document).keydown(function(event){
+    $("h1").text("YOU'RE GAME HAS BEEN STARTED");
+    gameActive = true;
+    console.log(gameActive);
+});
+
+if(gameActive){
+    generateRandomColor();
+    checkPlayerSequence();
+}
+
+function generateRandomColor(){
+    var randomColor = Math.floor(Math.random(colors)*4)+1;
+    sequence.push(randomColor);
+}
